387845e767a1dbcd76dab5dc9b4664fa8cf71c8d
Santiago Guevara <saguevaraq@ufpso.edu.co>
2024-03-08 11:51:06 -0500

    Avance de las Librerías

--- programacion2/src/main/java/bookstore/BooksStore.java
+++ programacion2/src/main/java/bookstore/BooksStore.java
@@ -9,9 +9,11 @@ public class BooksStore {
     public static void main(){
         Book book1 = new Book("Harry Potter",5,25,"Fantasía");
         Book book2 = new Book("El nombre del viento", 1, 28, "Fantasía");
+        Book book3 = new Book("Los Juegos del Hambre", 3, )
         //Presto el libro
         book1.lendBook();
         System.out.println(book1.isAvailable());
+        book2.lendBook();
         
     }
         
